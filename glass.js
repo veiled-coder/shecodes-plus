e2cc2af232b9678e15bd1515fd572fd7a14a23ef
BintReina <omobowalerohma1998@gmail.com>
2022-10-07 23:26:28 +0100

    round up temp to whole number

--- glass.js
+++ glass.js
@@ -1,12 +1,12 @@
 const toggleBtn = document.querySelector("#Toggle");
 let form = document.querySelector("#form--container");
 let searchDisplay = document.querySelector("#Search");
-let searchbtn = document.querySelector("#search-btn");
 
 let cityName = document.querySelector("h1");
 let temp = document.querySelector(".temp");
 let tempUnit = "metric";
 let apiKey = "c57ce4bd372d8c67eb7282dd25e41eae";
+
 let current = document.querySelector(".current-location");
 let humidity = document.querySelector(".humidity");
 let wind = document.querySelector(".wind");
@@ -16,6 +16,7 @@ function changeTheme() {
   let Body = document.documentElement;
   Body.className = toggleBtn.checked ? "dark" : "light";
 }
+toggleBtn.addEventListener("click", changeTheme);
 
 form.addEventListener("submit", weatherInfo);
 
@@ -23,17 +24,21 @@ function weatherInfo(e) {
   e.preventDefault();
   let searchValue = searchDisplay.value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=${tempUnit}&appid=${apiKey}`;
+
   axios.get(apiUrl).then(results);
-  function results(response) {
-    console.log(response.data.weather[0].main);
-    temp.innerHTML = response.data.main.temp;
-    cityName.innerHTML = searchValue;
-    humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-    wind.innerHTML = `Wind: ${response.data.wind.speed}km/hr`;
-    action.innerHTML = `${response.data.weather[0].main}`;
+  function results(weatherResponse) {
+    console.log(weatherResponse.data.weather[0].main);
+
+    weatherDetails(weatherResponse);
   }
 }
-
+function weatherDetails(response) {
+  temp.innerHTML = Math.round(response.data.main.temp);
+  cityName.innerHTML = response.data.name;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  wind.innerHTML = `Wind: ${response.data.wind.speed}km/hr`;
+  action.innerHTML = `${response.data.weather[0].main}`;
+}
 //current-location
 
 current.addEventListener("click", getCurrent);
@@ -43,13 +48,9 @@ function getCurrent() {
 function myResponse(location) {
   let apiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&units=${tempUnit}&appid=${apiKey}`;
   axios.get(apiUrlCurrent).then(currentResult);
-  function currentResult(response) {
-    console.log(response.data.main.temp);
-    cityName.innerHTML = `${response.data.name}`;
-    temp.innerHTML = ` ${response.data.main.temp}`;
-    humidity.innerHTML = `humidity: ${response.data.main.humidity}%`;
-    wind.innerHTML = `Wind: ${response.data.wind.speed}km/hr`;
-    action.innerHTML = `${response.data.weather[0].main}`;
+  function currentResult(weatherResponse) {
+    console.log(weatherResponse.data.main.temp);
+    weatherDetails(weatherResponse);
   }
 }
 //time
@@ -76,8 +77,8 @@ let week = [
 dateText.innerHTML = `${week[weekday]} 
 ${hour < 10 ? "0" + hour : hour}:${min < 10 ? "0" + min : min}`;
 
-let fah = document.querySelector(".fah");
-let cel = document.querySelector(".cel");
+// let fah = document.querySelector(".fah");
+// let cel = document.querySelector(".cel");
 
 // fah.addEventListener("click", fahConvert);
 // cel.addEventListener("click", celConvert);
